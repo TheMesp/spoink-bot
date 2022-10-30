3a38b4f931f01f758e5f97f0ea6841ae0c21714c
root <dloewen481@gmail.com>
2022-10-30 19:20:48 +0000

    evo tracking

--- commands/rng.rb
+++ commands/rng.rb
@@ -20,7 +20,7 @@ POKEDEX_BANLIST = %w(
   eternatus
   eternatus-eternamax
   genesect
-  giratina
+  giratina-altered
   giratina-origin
   groudon
   ho-oh
@@ -54,45 +54,78 @@ POKEDEX_BANLIST = %w(
   zekrom
   zygarde
 )
-# given a pokeAPI evolution-chain id, determine the final element of the chain.
-def get_final_evo(id)
+
+
+# recursively returns valid evos
+def trace_evo_tree(chain, base_form, passed_base_form)
+  # stupid edge cases
+  return ['urshifu-rapid-strike'] if base_form == 'kubfu'
+  return ['you decide'] if chain['species']['name'] == 'eevee'
+  return ['lol, lmao'] if chain['species']['name'] == 'cosmog'
+  output = []
+  if passed_base_form && !POKEDEX_BANLIST.include?(chain['species']['name'])
+   output += [chain['species']['name'].capitalize]
+  end
+  chain['evolves_to'].each do |evo|
+    output += trace_evo_tree(evo, base_form, passed_base_form || chain['species']['name'] == base_form)
+  end
+  return output
+end
+
+# given a pokeAPI evolution-chain id and pokemon name, determine all valid evolutions
+def get_evos(id, name)
   response = `curl -s https://pokeapi.co/api/v2/evolution-chain/#{id}`
   response = JSON.parse(response)
-  return ''
+  output = []
+  output += trace_evo_tree(response['chain'], name, false)
+  return output
 end
+
 def setup_rng_commands(bot)
   
   bot.application_command(:roll) do |event|
-    num = event.options['num'].to_i
-    output_names = []
-    for i in 1..num do
-      poke_name = ''
-      loop do
-        next_id = rand(1..MAX_POKEDEX_NUM)
-        response = `curl -s https://pokeapi.co/api/v2/pokemon-species/#{next_id}`
-        response = JSON.parse(response)
-        temp_name = ""
+    if !event.options['num'] || event.options['num'].to_i < 1 || event.options['num'] > 6
+      event.respond(content:"Invalid number. Only 1-6, please.", ephemeral: true)
+    else
+      event.respond(content:"Generating, please wait...")
+      num = event.options['num'].to_i
+      output_names = []
+      output_ids = []
+      for i in 1..num do
+        poke_name = ''
         loop do
-          temp_name = response['varieties'].sample['pokemon']['name']
-          unless temp_name.include?('mega') || temp_name.include?('gmax') || temp_name.include?('totem')
+          next_id = rand(1..MAX_POKEDEX_NUM)
+          response = `curl -s https://pokeapi.co/api/v2/pokemon-species/#{next_id}`
+          response = JSON.parse(response)
+          temp_name = ""
+          loop do
+            temp_name = response['varieties'].sample['pokemon']['name']
+            unless temp_name.include?('mega') || temp_name.include?('gmax') || temp_name.include?('totem')
+              break
+            end
+          end     
+          if POKEDEX_BANLIST.include? temp_name
+            poke_name << "~~#{temp_name.capitalize}~~ "
+          else
+            poke_name << temp_name.capitalize
+            if response['evolution_chain']
+              evos = get_evos(response['evolution_chain']['url'].split('evolution-chain/')[1].to_i, temp_name)
+              poke_name << " (#{evos.join(", ")})" unless evos.empty?
+            end
+            output_names << poke_name
+            output_ids << next_id
             break
           end
-        end     
-        if POKEDEX_BANLIST.include? temp_name
-          poke_name << "~~##{next_id}: #{temp_name.capitalize}~~ "
-        else
-          poke_name << "##{next_id}: #{temp_name.capitalize}"
-          poke_name << get_final_evo(response['evolution-chain'].split('evolution-chain/')[1].to_i) if response['evolution-chain']
-          output_names << poke_name
-          break
         end
+        
       end
-      
+      response_embed = Discordrb::Webhooks::Embed.new(
+        title: "Team of #{num}:"
+      )
+      output_names.each_with_index do |output, i|
+        response_embed.add_field(name: "##{output_ids[i]}", value: output)
+      end
+      event.edit_response(embeds:[response_embed.to_hash])
     end
-    response_embed = Discordrb::Webhooks::Embed.new(
-      title: "Team of #{num}:",
-      description: output_names.join("\n")
-    )
-    event.respond(embeds:[response_embed.to_hash])
   end
 end
