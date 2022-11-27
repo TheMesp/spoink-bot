0d1b4b3015c91da08e3eff2cf203780fcaf0cd05
root <dloewen481@gmail.com>
2022-11-27 01:06:05 +0000

    Globalized slash commands

--- command_registry.rb
+++ command_registry.rb
@@ -1,8 +1,8 @@
 def register_commands(bot)
-  bot.register_application_command(:pokemon, 'Prints basic information about a pokemon, including type and weaknesses', server_id: 662470757716197428) do |cmd|
+  bot.register_application_command(:pokemon, 'Prints basic information about a pokemon, including type and weaknesses') do |cmd|
     cmd.string('pokemon', 'The pokemon to look up')
   end
-  bot.register_application_command(:roll, 'Roll a pokemon team', server_id: 662470757716197428) do |cmd|
+  bot.register_application_command(:roll, 'Roll a pokemon team') do |cmd|
     cmd.integer('num', 'The number of pokemon to roll')
   end
 end
