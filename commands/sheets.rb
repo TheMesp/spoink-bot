e71a6c9c69f1078d5596c0022595751e18634edf
root <dloewen481@gmail.com>
2026-07-20 03:46:14 +0000

    added s14 playoff sheet

--- commands/sheets.rb
+++ commands/sheets.rb
@@ -6,6 +6,6 @@ def setup_sheets_commands(bot)
     event.respond(content: "https://docs.google.com/spreadsheets/d/108PwlIrNHNb0TgfR7ByPrd42DY2ojEFr_aN_7rlw3Ag", ephemeral: event.options['hide'])
   end
   bot.application_command(:sheets).subcommand(:schedule) do |event|
-    event.respond(content: "Coming soon", ephemeral: event.options['hide'])
+    event.respond(content: "https://docs.google.com/spreadsheets/d/1ogPhQmyCUYFnQ1x2QsykNJVvDRedfww0ihVv9QGAsLY/edit?usp=sharing", ephemeral: event.options['hide'])
   end
 end
